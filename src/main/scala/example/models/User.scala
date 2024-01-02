53a505c6e25c6ad6fdfb7404d2e1f5abcea4b74b
Wenshuai Hou <wenhoujx@gmail.com>
2024-01-01 21:04:45 -0500

    add tests

--- src/main/scala/example/models/User.scala
+++ src/main/scala/example/models/User.scala
@@ -11,6 +11,5 @@ final case class User(
 // companion object
 object User:
   def make(name: String): UIO[User] =
-    ???
+    UserId.random.map(User(_, name))
   given codec: JsonCodec[User] = DeriveJsonCodec.gen[User]
-  
--- src/test/scala/example/models/TestModelSpec.scala
+++ src/test/scala/example/models/TestModelSpec.scala
@@ -3,6 +3,7 @@ package example.models
 import zio.*
 import zio.test.*
 import zio.test.Assertion.*
+import zio.json.*
 
 object TestModelsSpec extends ZIOSpecDefault:
   override def spec = suite("TestModels")(
@@ -10,5 +11,11 @@ object TestModelsSpec extends ZIOSpecDefault:
       for {
         res <- ZIO.succeed(42)
       } yield assert(res)(equalTo(42))
+    },
+    test("user round-trip") {
+      for
+        user <- User.make("wen")
+        newUser = user.toJson.fromJson[User]
+      yield assert(Right(user))(equalTo(newUser))
     }
   )
