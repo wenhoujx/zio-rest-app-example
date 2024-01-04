a0cbeace2f6c7d825ccd6d8a8cc0e1b8c43c1ae2
Wenshuai Hou <wenhoujx@gmail.com>
2024-01-04 13:43:39 -0500

    refactor specs test to have better code coverage.

--- src/test/scala/example/servers/MarriageServiceSpec.scala
+++ src/test/scala/example/servers/MarriageServiceSpec.scala
@@ -9,6 +9,7 @@ import zio.*
 import zio.test.*
 import zio.test.Assertion.*
 import example.errors.AppError
+import example.routes.*
 
 object MarriageServiceSpec extends ZIOSpecDefault:
   // somehow sbt compiler can't get the types during runtime.
@@ -20,25 +21,36 @@ object MarriageServiceSpec extends ZIOSpecDefault:
     suite("MarriageServiceSpec")(
       allTests.provide(
         layers.marriageDbLayer,
-        MarriageServer.live
+        layers.userDbLayer,
+        InRamUserServer.live,
+        MarriageServer.live,
+        MarriageRoutes.live,
+        UserRoutes.live
       )
     )
+  def createUsers(n: Int): ZIO[UserRoutes, Throwable, List[User]] = {
+    ZIO.loop(0)(_ < n, _ + 1)(n =>
+      ZIO.serviceWithZIO[UserRoutes](_.service.createUser(s"user-$n"))
+    )
+  }
+  val getMarriageService: ZIO[MarriageRoutes, Throwable, MarriageService] =
+    ZIO.environmentWith(_.get.service)
 
   lazy val allTests = suite("test marriage service happy path")(
     test("create marriage") {
       for
-        marriageService <- ZIO.service[MarriageService]
-        id1 <- UserId.random
-        id2 <- UserId.random
+        marriageService <- getMarriageService
+        users <- createUsers(2)
+        (id1, id2) = (users(0).id, users(1).id)
         m <- marriageService.marry(id1, id2)
       yield assert(m.userA)(isOneOf(List(id1, id2))) &&
         assert(m.userB)(isOneOf(List(id1, id2)))
     },
     test("query marriage status") {
       for
-        marriageService <- ZIO.service[MarriageService]
-        id1 <- UserId.random
-        id2 <- UserId.random
+        marriageService <- getMarriageService
+        users <- createUsers(2)
+        List(id1, id2) = (users.take(2)).map(_.id)
         m <- marriageService.marry(id1, id2)
         getM1 <- marriageService.marriageStatus(id1)
         getM2 <- marriageService.marriageStatus(id2)
@@ -46,9 +58,9 @@ object MarriageServiceSpec extends ZIOSpecDefault:
     },
     test("test divorce") {
       for
-        marriageService <- ZIO.service[MarriageService]
-        id1 <- UserId.random
-        id2 <- UserId.random
+        marriageService <- getMarriageService
+        users <- createUsers(2)
+        List(id1, id2) = users.map(_.id)
         m <- marriageService.marry(id1, id2)
         _ <- marriageService.divorce(id1, id2)
         m1 <- marriageService.marriageStatus(id1)
@@ -57,18 +69,17 @@ object MarriageServiceSpec extends ZIOSpecDefault:
     },
     test("self marry banned") {
       val v = for
-        marriageService <- ZIO.service[MarriageService]
-        id1 <- UserId.random
+        marriageService <- getMarriageService
+        id1 <- createUsers(1).map(_.head.id)
         res <- marriageService.marry(id1, id1)
       yield res
       assertZIO(v.exit)(fails(equalTo(AppError.CannotSelfMarryError)))
     },
     test("can't marry one that's already married") {
       val v = for
-        marriageService <- ZIO.service[MarriageService]
-        id1 <- UserId.random
-        id2 <- UserId.random
-        id3 <- UserId.random
+        marriageService <- getMarriageService
+        users <- createUsers(3)
+        List(id1, id2, id3) = users.map(_.id)
         _ <- marriageService.marry(id1, id2)
         res <- marriageService.marry(id1, id3)
       yield res
@@ -76,17 +87,57 @@ object MarriageServiceSpec extends ZIOSpecDefault:
         fails(isSubtype[AppError.AlreadyMarriedError](anything))
       )
     },
+    test("divorce unmarried couple") {
+      val v = for
+        marriageService <- getMarriageService
+        users <- createUsers(2)
+        List(id1, id2) = users.map(_.id)
+        m <- marriageService.divorce(id1, id2)
+      yield m
+      assertZIO(v.exit)(
+        fails(isSubtype[AppError.NotMarriedError](anything))
+      )
+    },
     test("can't remarry the same person") {
       val v = for
-        marriageService <- ZIO.service[MarriageService]
-        id1 <- UserId.random
-        id2 <- UserId.random
+        marriageService <- getMarriageService
+        users <- createUsers(2)
+        List(id1, id2) = users.map(_.id)
         _ <- marriageService.marry(id1, id2)
         m <- marriageService.marry(id1, id2)
       yield m
       assertZIO(v.exit)(
         fails(isSubtype[AppError.AlreadyMarriedError](anything))
       )
-
+    },
+    test("missing user can't get married") {
+      val v = for
+        marriageService <- getMarriageService
+        id <- Random.nextUUID.map(UserId(_))
+        m <- marriageService.marry(id, id)
+      yield m
+      assertZIO(v.exit)(
+        fails(isSubtype[AppError.MissingUserError](anything))
+      )
+    },
+    test("missing user can't get divorce") {
+      val v = for
+        marriageService <- getMarriageService
+        id <- Random.nextUUID.map(UserId(_))
+        m <- marriageService.divorce(id, id)
+      yield m
+      assertZIO(v.exit)(
+        fails(isSubtype[AppError.MissingUserError](anything))
+      )
+    },
+    test("missing user can't get marriage status") {
+      val v = for
+        marriageService <- getMarriageService
+        id <- Random.nextUUID.map(UserId(_))
+        m <- marriageService.marriageStatus(id)
+      yield m
+      assertZIO(v.exit)(
+        fails(isSubtype[AppError.MissingUserError](anything))
+      )
     }
   )
--- src/test/scala/example/servers/UserServerSpec.scala
+++ src/test/scala/example/servers/UserServerSpec.scala
@@ -1,26 +1,31 @@
 package example.servers
 
 import example.services.UserService
+import example.services.MarriageService
 import example.servers.*
 import example.models.*
 
 import zio.*
 import zio.test.*
 import zio.test.Assertion.*
+import example.routes.*
+import example.errors.AppError
 
 object UserServerSpec extends ZIOSpecDefault:
+  val getUserService: ZIO[UserRoutes, Throwable, UserService] =
+    ZIO.environmentWith(_.get.service)
   override def spec: Spec[TestEnvironment, Throwable] = {
     suite("UserServer")(
       test("create User then get") {
         for
-          userService <- ZIO.service[UserService]
+          userService <- getUserService
           user <- userService.createUser("wen")
           get <- userService.getUser(user.id)
         yield assert(Some(user))(equalTo(get))
       },
       test("get all") {
         for
-          userService <- ZIO.service[UserService]
+          userService <- getUserService
           user1 <- userService.createUser("Wen")
           user2 <- userService.createUser("Test")
           allUsers <- userService.getAll()
@@ -30,7 +35,7 @@ object UserServerSpec extends ZIOSpecDefault:
       },
       test("update") {
         for
-          userService <- ZIO.service[UserService]
+          userService <- getUserService
           user1 <- userService.createUser("Wen")
           user2 <- userService.createUser("Test")
           _ <- userService.updateUser(user1.id, UpdateUser(Some("WWen")))
@@ -51,14 +56,31 @@ object UserServerSpec extends ZIOSpecDefault:
       },
       test("delete User") {
         for
-          userService <- ZIO.service[UserService]
+          userService <- getUserService
           user <- userService.createUser("wen")
           _ <- userService.deleteUser(user.id)
           allUsers <- userService.getAll()
         yield assert(allUsers)(isEmpty)
+      },
+      test("can't delete married user") {
+        val v = for
+          userService <- getUserService
+          marriageService <- ZIO.service[MarriageService]
+          user1 <- userService.createUser("user1")
+          user2 <- userService.createUser("user2")
+          m <- marriageService.marry(user1.id, user2.id)
+          res <- userService.deleteUser(user1.id)
+        yield res
+        assertZIO(v.exit)(
+          fails(isSubtype[AppError.CannotDeleteMarriedUserError](anything))
+        )
+
       }
     )
   }.provide(
     layers.userDbLayer,
-    InRamUserServer.live
+    InRamUserServer.live,
+    layers.marriageDbLayer,
+    MarriageServer.live,
+    UserRoutes.live
   )
